84f141fa9862ec15388b24277f0e4b5eb0defe1a
bswan0113 <zkoiu@naver.com>
2022-10-25 16:36:40 +0900

    good

--- src/day01/VariableEx01.java
+++ src/day01/VariableEx01.java
@@ -1,44 +1,44 @@
-package day01;
-
-public class VariableEx01 {
-
-	public static void main(String[] args) {
-		/* - 변수의 선언
-		 *  자료형 변수명; 
-		 *  or  자료형 변수명 = 초기값;
-		 *  - 자료형 
-		 */
-	
-		boolean isEven = false;
-		char operator = '+';
-		int age;
-		age = 30;
-		float pi1 = 3.141592f;
-		double pi2 = 3.1415921472084111111;
-		String str = "Hi";
-		String str2 = new String();
-		str2 = "Hi";
-		
-		// 사과의 갯수를 저장하기 위한 변수
-		int appleCount;
-		
-		// 학생 평균성적을 저장하기 위한 변수를 선언해 보세요.
-		double  scoreAvg;
-		int num;
-//		int num; // Duplicate local variable num : 중복된 변수 선언시 발생하는 에러
-		// 대소문자 구분됨 -> num  != Num
-		// int 1Num-> 숫자로 시작 불가능
-		int _num1, $num;
-//		int  !num2; // _,$제외 특수문자 사용불가
-//		int int; 예약어 사용불가
-
-	/*
-	 * 관례
-	 * - 변수는 소문자로 시작 
-	 * - 낙타등 표기법사용
-	 * - 클래스와 인터페이스는 대문자로 시작
-	 */
-		
-	}
-
-}
+package day01; 
+ 
+public class VariableEx01 { 
+ 
+	public static void main(String[] args) { 
+		/* - 변수의 선언 
+		 *  자료형 변수명;  
+		 *  or  자료형 변수명 = 초기값; 
+		 *  - 자료형  
+		 */ 
+	 
+		boolean isEven = false; 
+		char operator = '+'; 
+		int age; 
+		age = 30; 
+		float pi1 = 3.141592f; 
+		double pi2 = 3.1415921472084111111; 
+		String str = "Hi"; 
+		String str2 = new String(); 
+		str2 = "Hi"; 
+		 
+		// 사과의 갯수를 저장하기 위한 변수 
+		int appleCount; 
+		 
+		// 학생 평균성적을 저장하기 위한 변수를 선언해 보세요. 
+		double  scoreAvg; 
+		int num; 
+//		int num; // Duplicate local variable num : 중복된 변수 선언시 발생하는 에러 
+		// 대소문자 구분됨 -> num  != Num 
+		// int 1Num-> 숫자로 시작 불가능 
+		int _num1, $num; 
+//		int  !num2; // _,$제외 특수문자 사용불가 
+//		int int; 예약어 사용불가 
+ 
+	/* 
+	 * 관례 
+	 * - 변수는 소문자로 시작  
+	 * - 낙타등 표기법사용 
+	 * - 클래스와 인터페이스는 대문자로 시작 
+	 */ 
+		 
+	} 
+ 
+} 
--- src/day02/ScannerEx01.java
+++ src/day02/ScannerEx01.java
@@ -1,31 +1,31 @@
-package day02;
-
-import java.util.Scanner;
-
-public class ScannerEx01 {
-
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("정수를 입력해주세요. : ");
-		int num = scan.nextInt();
-		System.out.println(num + 2);
-		System.out.println("실수를 입력하세요 : ");
-		double num2 = scan.nextDouble();
-		System.out.println(num2*3);
-		
-		System.out.println("정수 A와 정수 B를 입력하세요 : ");
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		
-		System.out.println(a*b);
-		
-		scan.close();
-		
-		
-	
-		
-
-	}
-
-}
+package day02; 
+ 
+import java.util.Scanner; 
+ 
+public class ScannerEx01 { 
+ 
+	public static void main(String[] args) { 
+		 
+		Scanner scan = new Scanner(System.in); 
+		System.out.println("정수를 입력해주세요. : "); 
+		int num = scan.nextInt(); 
+		System.out.println(num + 2); 
+		System.out.println("실수를 입력하세요 : "); 
+		double num2 = scan.nextDouble(); 
+		System.out.println(num2*3); 
+		 
+		System.out.println("정수 A와 정수 B를 입력하세요 : "); 
+		int a = scan.nextInt(); 
+		int b = scan.nextInt(); 
+		 
+		System.out.println(a*b); 
+		 
+		scan.close(); 
+		 
+		 
+	 
+		 
+ 
+	} 
+ 
+} 
