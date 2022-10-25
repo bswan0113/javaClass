c263fd5976be064e813bbc0d4abb0a26d3613643
bswan0113 <zkoiu@naver.com>
2022-10-25 16:34:21 +0900

    good

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class HelloWorld {
+// public -> 접근제어자
+	public static void main(String[] args) { // 프로그램이 시작하려면 main이 있어야함.
+		
+		System.out.println("Hello World!");
+		//System.out.println(); 콘솔에 원하는 값을 출력하는 기능
+		//는 한줄 주석 , 나오는  순간부터 해당 줄 끝까지
+		/*
+		 * 여러줄 주석
+		 */ 
+		
+		/**
+		 *  여러줄 주석, 기능을 설명할 때 활용 -> 기능 호버시 설명문구 출력
+		 */
+		
+		
+	}
+
+}
