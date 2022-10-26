8d1eb93999e64932bf4c2b35cc0f9c36325084b1
bswan0113 <zkoiu@naver.com>
2022-10-26 21:49:00 +0900

    add

--- src/day03/ForEx05.java
+++ src/day03/ForEx05.java
@@ -12,11 +12,22 @@ public class ForEx05 {
 		
 
 		for(i=1; i<=10; i++) {
+			
 			sum +=i;
+			System.out.println(1 + "부터" + i + "의 합은" + sum );
 		} // 시그마
 		
 		System.out.println(sum);
 
+		
+		//Teacher Ver.
+		
+		int j, Sum;
+		for(j=1, Sum=0; j<=10; j++) {
+			Sum=Sum+j;
+		
+		}
+		System.out.println("1부터 10까지의 합은" + Sum + " 입니다.");
 	}
 
 }
