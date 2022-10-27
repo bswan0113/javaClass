571177b6357f3c5091208c8e2eb7c8f29de488bc
bswan0113 <zkoiu@naver.com>
2022-10-27 21:39:36 +0900

    mine

--- src/mine/Prime.java
+++ src/mine/Prime.java
@@ -5,7 +5,7 @@ public class Prime {
 	public static void main(String[] args) {
 		
 		int count=0;
-        for(int i=101527 ; i<112589  ; i++){
+        for(int i=1 ;  ; i++){
             for(int j=2; j<=i; j++){
                 if(i%j == 0){
                     count ++;
